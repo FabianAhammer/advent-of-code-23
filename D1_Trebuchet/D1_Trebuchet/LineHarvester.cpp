35aa20c6af303751bd46797dfe41795cf67a7dce
Fabian Ahammer <tf3kik@gmail.com>
2023-12-01 16:36:25 +0100

    V1

---
+++ D1_Trebuchet/D1_Trebuchet/LineHarvester.cpp
@@ -0,0 +1,28 @@
+﻿#include "LineHarvester.h"
+
+
+int LineHarvester::parseLineToInts(std::string inputLine)
+{
+    int firstNumber = -1;
+    int secondNumber = -1;
+    for (const char value : inputLine)
+    {
+        if (std::isdigit(value))
+        {
+            if (firstNumber == -1)
+            {
+                firstNumber = value - '0';
+                secondNumber = firstNumber;
+            }
+            else
+            {
+                secondNumber = value - '0';
+            }
+        }
+    }
+    if (firstNumber == -1)
+    {
+        return 0;
+    }
+    return firstNumber * 10 + secondNumber;
+}
---
+++ D1_Trebuchet/D1_Trebuchet/LineHarvester.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <string>
+
+class LineHarvester
+{
+public:
+    static int parseLineToInts(std::string inputLine);
+};
